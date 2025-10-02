86f1ff0859b9855668a1ec27727d52d807a01818
Maleeharahim23 <maleeha.bhatti@theclevercoders.com>
2025-10-02 14:04:02 +0500

    Update Playwright config to run headless in CI

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,8 @@
 const config = {
   timeout: 120 * 1000,
   use: {
-    headless: false,
+    // 👇 Run headless in CI, but headed locally
+    headless: process.env.CI ? true : false,
     viewport: { width: 1920, height: 1080 },
     actionTimeout: 30 * 1000,
     video: 'retain-on-failure',
@@ -12,4 +13,5 @@ const config = {
   testDir: './',
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
 };
+
 module.exports = config;
