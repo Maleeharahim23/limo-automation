849075afca6cd5c07ad749248d41180f9570e8ad
Maleeharahim23 <maleeha.bhatti@theclevercoders.com>
2025-10-13 14:13:05 +0500

    Trigger CI run

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,16 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
     timeout: 120 * 1000,
+    testDir: './',
     use: {
         headless: true,
-        viewport: { width: 1920, height: 1080 }, // ⬅️ force page viewport
+        viewport: { width: 1920, height: 1080 },
         launchOptions: {
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--window-size=1920,1080', // ⬅️ enforce browser window size
-            ],
+            args: ['--window-size=1920,1080'],
         },
         video: 'on',
         screenshot: 'on',
     },
-    testDir: './',
     reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
 };
 
