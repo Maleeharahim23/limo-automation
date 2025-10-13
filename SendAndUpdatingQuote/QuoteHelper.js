e25a0c39deab72483920844d1ef2bf00c325c132
Maleeharahim23 <maleeha.bhatti@theclevercoders.com>
2025-10-13 12:47:02 +0500

    Trigger CI run

--- SendAndUpdatingQuote/QuoteHelper.js
+++ SendAndUpdatingQuote/QuoteHelper.js
@@ -37,6 +37,7 @@ class QuoteHelper {
         await this.sendQuote.clickNextButtonAfterEnteringPriceDetails();
         await this.sendQuote.clickLeg2TabBtn();
         await this.sendQuote.removeMainPrice();
+
         await this.sendQuote.enterMainPrice("200");
         await this.sendQuote.clickAdditionalChargesBtn();
         await this.sendQuote.enterPayment("20");
