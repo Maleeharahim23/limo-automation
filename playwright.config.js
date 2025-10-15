75e70d55d3cc05543522373ebb216afcaac4e1ca
Maleeharahim23 <maleeha.bhatti@theclevercoders.com>
2025-10-15 14:20:01 +0500

    Trigger CI run

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,16 @@
+// playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-    timeout: 120 * 1000,
-    testDir: './',
-    use: {
-        headless: true,
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-            args: ['--window-size=1920,1080'],
-        },
-        video: 'on',
-        screenshot: 'on',
-        browserName: 'chromium',
-        channel: process.env.CI ? 'msedge' : 'chrome',
-    },
-    reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
+  timeout: 120 * 1000,
+  use: {
+    headless:true,
+    viewport: { width: 1280, height: 720 },
+    // actionTimeout: 30 * 1000,
+    video: 'on',
+    screenshot: 'on',
+  },
+  testDir: './',
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
 };
 
 module.exports = config;
